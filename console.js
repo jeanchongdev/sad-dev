dc1f484dc68862484b98792241198c3328ae2447
Jean Pierre Chong <126737435+jeanchong15@users.noreply.github.com>
2023-10-10 14:42:33 -0500

    Revert "changed something"

--- console.js
+++ console.js
@@ -1,6 +1,10 @@
-console.log("Hello World");
+// hey this  is a comment
+console.log("Hello World") 
+console.log("Hello World") // 2nd time! 
 
-function main(){
-  console.log(1);
-}
-main();
+funtion main {
+  return null;
+} 
+main() 
+
+console.log(main)
