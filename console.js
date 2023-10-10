24b82095c25f76c46974d9487106c31f8a27c279
Jean Pierre Chong <grarenajeanpro@gmail.com>
2023-10-10 14:38:57 -0500

    changed somet
    hello

--- console.js
+++ console.js
@@ -1,10 +1,6 @@
-// hey this  is a comment
-console.log("Hello World") 
-console.log("Hello World") // 2nd time! 
+console.log("Hello World");
 
-funtion main {
-  return null;
-} 
-main() 
-
-console.log(main)
+function main(){
+  console.log(1);
+}
+main();
